da21a36e6b388d9195eb17ef3abf6cab778da7f1
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-07-22 16:40:01 -0300

    Add files via upload

--- webserver/views.py
+++ webserver/views.py
@@ -339,7 +339,7 @@ def usuarios(request):
         #"form": form,
         "results": results,
     })
-
+@login_required
 def cadastrarUsuario(request):
     if request.method == "POST":
         form = UserForm(request.POST)
